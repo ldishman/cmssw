41a901176e08ec587422054f09e6267bab893ca8
Lacey Dishman <ldishman@lxplus712.cern.ch>
2025-06-23 20:23:50 +0200

    Change BunchXingWindow and NumberOfSimulatedTracks histo titles (testing, worked)

--- Validation/SiTrackerPhase2V/plugins/Phase2TrackerValidateDigi.cc
+++ Validation/SiTrackerPhase2V/plugins/Phase2TrackerValidateDigi.cc
@@ -376,7 +376,7 @@ void Phase2TrackerValidateDigi::bookHistograms(DQMStore::IBooker& ibooker,
   std::stringstream HistoName;
 
   HistoName.str("");
-  HistoName << "NumberOfSimulatedTracks";
+  HistoName << "NumberOfSimulatedTracksHELLOWORLD";
   nSimulatedTracks = ibooker.book1D(HistoName.str(), HistoName.str(), 501, -0.5, 500.5);
 
   HistoName.str("");
@@ -870,7 +870,7 @@ void Phase2TrackerValidateDigi::bookLayerHistos(DQMStore::IBooker& ibooker, unsi
       local_mes.SimHitDz = nullptr;
 
     HistoName.str("");
-    HistoName << "BunchXingWindow";
+    HistoName << "BunchXingWindowHELLOWORLD";
     local_mes.BunchXTimeBin = ibooker.book1D(HistoName.str(), HistoName.str(), 8, -5.5, 2.5);
 
     HistoName.str("");
